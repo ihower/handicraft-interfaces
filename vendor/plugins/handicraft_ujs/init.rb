8617fc5271204b8138508d5b9456aff977d33ceb
Kang-min Liu <gugod@gugod.org>
2009-05-21 18:27:49 +0800

    remove plugins

--- vendor/plugins/handicraft_ujs/init.rb
+++
@@ -1,4 +0,0 @@
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-ui','jquery.metadata','jquery.form','handicraft']
-ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
-require 'jquery_for_rjs'
-ActionController::Base.helper Handicraft::UjsHelper
